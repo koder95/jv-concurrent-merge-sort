b3d390e753e787f277f0080e90dceb2b163ba634
Kamil Jan Mularski <kammul@outlook.com>
2026-02-05 12:38:47 +0100

    My solution

--- src/main/java/mate/academy/MergeSortAction.java
+++ src/main/java/mate/academy/MergeSortAction.java
@@ -1,8 +1,28 @@
 package mate.academy;
 
-public class MergeSortAction {
+import java.util.Arrays;
+import java.util.concurrent.RecursiveAction;
+
+public class MergeSortAction extends RecursiveAction {
+
+    private final int[] array;
 
     public MergeSortAction(int[] array) {
+        this.array = array;
+    }
 
+    @Override
+    protected void compute() {
+        if (array.length > 2) {
+            MergeSortTask subTask = new MergeSortTask(Arrays.copyOf(array, array.length));
+            subTask.fork();
+            int[] tmp = subTask.join();
+            System.arraycopy(tmp, 0, array, 0, tmp.length);
+        } else if (array.length == 2) {
+            int min = Math.min(array[0], array[1]);
+            int max = Math.max(array[0], array[1]);
+            array[0] = min;
+            array[1] = max;
+        }
     }
 }
